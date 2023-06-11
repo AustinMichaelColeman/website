e1d3eb7bdd5274571808fd7577e82550dfbbb8ce
Austin Coleman <austinmichaelcoleman@gmail.com>
2023-06-10 21:52:54 -0700

    Updated blog page to display blog posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import blogPosts from '../data/blogPosts';
 
 const Blog = () => {
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold mb-4">Blog</h1>
-      <p>Welcome to the blog!</p>
+      {blogPosts.map((post) => (
+        <div key={post.id} className="mb-8">
+          <h2 className="text-xl font-bold mb-2">{post.title}</h2>
+          <p className="text-gray-500 mb-2">{post.date}</p>
+          <p>{post.content}</p>
+        </div>
+      ))}
     </div>
   );
 };
