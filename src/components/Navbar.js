ada08735154628afede38c6cfea0e66c187c76cc
Austin Coleman <austinmichaelcoleman@gmail.com>
2023-06-10 21:44:21 -0700

    Added a new Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/blog">Blog</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
