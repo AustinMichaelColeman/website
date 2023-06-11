0c5a5a7e5c4d6d148208631d23f396069c767915
Austin Coleman <austinmichaelcoleman@gmail.com>
2023-06-10 21:49:59 -0700

    Updated contact page with basic styles

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,22 +2,22 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact</h1>
-      <form>
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Contact</h1>
+      <form className="space-y-4">
         <label>
           Name:
-          <input type="text" name="name" />
+          <input type="text" name="name" className="block w-full p-2 border border-gray-300 rounded" />
         </label>
         <label>
           Email:
-          <input type="text" name="email" />
+          <input type="text" name="email" className="block w-full p-2 border border-gray-300 rounded" />
         </label>
         <label>
           Message:
-          <textarea name="message" />
+          <textarea name="message" className="block w-full p-2 border border-gray-300 rounded" />
         </label>
-        <input type="submit" value="Submit" />
+        <input type="submit" value="Submit" className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700" />
       </form>
     </div>
   );
