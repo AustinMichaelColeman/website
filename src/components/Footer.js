1048fcad0cccf904d37323dea167a90ea1fd1556
Austin Coleman <austinmichaelcoleman@gmail.com>
2023-06-10 21:53:34 -0700

    Added a new Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-200 p-4 mt-8">
+      <p className="text-center text-gray-500">
+        © 2023 Website. Created by DevDuetAI.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
