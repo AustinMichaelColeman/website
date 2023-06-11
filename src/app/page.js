d40272e083116283cb02f11d430c683590b07171
Austin Coleman <austinmichaelcoleman@gmail.com>
2023-06-10 21:51:46 -0700

    Updated homepage with more content

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,16 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Website</h1>
-      <h2>
-        A website created entirely by DevDuetAI, a plugin for ChatGPT and for
-        GitHub.
-      </h2>
+      <h1 className="text-2xl font-bold mb-4">Welcome to the Website!</h1>
+      <p className="mb-4">
+        This website was created entirely by DevDuetAI, a plugin for ChatGPT and for GitHub.
+      </p>
+      <h2 className="text-xl font-bold mb-2">Features:</h2>
+      <ul className="list-disc list-inside">
+        <li>Blog section to share updates and news</li>
+        <li>Contact form to send messages directly from the website</li>
+        <li>Navbar for easy navigation between different pages</li>
+      </ul>
     </main>
   );
 }
