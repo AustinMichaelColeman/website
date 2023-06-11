15ac974cb1ff5f0220f76963944d8111f2040983
Austin Coleman <austinmichaelcoleman@gmail.com>
2023-06-10 21:52:31 -0700

    Added blogPosts data

---
+++ src/data/blogPosts.js
@@ -0,0 +1,16 @@
+const blogPosts = [
+  {
+    id: 1,
+    title: 'First Blog Post',
+    content: 'This is the first blog post.',
+    date: '2023-06-10'
+  },
+  {
+    id: 2,
+    title: 'Second Blog Post',
+    content: 'This is the second blog post.',
+    date: '2023-06-11'
+  },
+];
+
+export default blogPosts;
