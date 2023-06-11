4daf809e2c77ac4b3cfc18f95283007d45eaecc2
Austin Coleman <austinmichaelcoleman@gmail.com>
2023-06-10 21:49:38 -0700

    Updated blog page with basic styles

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Blog = () => {
   return (
-    <div>
-      <h1>Blog</h1>
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Blog</h1>
       <p>Welcome to the blog!</p>
     </div>
   );
