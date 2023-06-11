7e6dafc1e96aee944e42236a02477782031987e8
Austin Coleman <AustinMichaelColeman@gmail.com>
2023-06-10 21:31:31 -0700

    Create initial skeleton of site for DevDuetAI to continue

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Website</h1>
+      <h2>
+        A website created entirely by DevDuetAI, a plugin for ChatGPT and for
+        GitHub.
+      </h2>
+    </main>
+  );
+}
